43b0a11f888ed2bc24a8dc2088dbea0632c6ae94
mlambe05 <mark.lambert@fiscal.treasury.gov>
2020-03-02 14:01:39 -0500

    added connectivity test api call in binance client

--- BinanceClient.py
+++ BinanceClient.py
@@ -1,5 +1,9 @@
-import response
+import requests
 import DateTime
+
+
+baseApiUrl = 'https://api.binance.us'
+connectivityEndpoint = '/api/v3/ping'
 
 
 
@@ -7,6 +11,17 @@ def getHistoricalData():
     returnVal = 'abcd'
     return returnVal
     
+def testConnectivity():
+    url = baseApiUrl + connectivityEndpoint
+    try:
+        response = requests.get(url)
+        if(response.status_code == 200):
+            return "connection is valid"
+        else:
+            return "error connecting to Binance exchange"
+    
+    except:
+        return 'exception thrown while trying to ping binance exchange'
 
 
 def makePurchase():
