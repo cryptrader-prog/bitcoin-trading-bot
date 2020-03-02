6b378adff788e6706eab8c2dd20b524fe783d8fd
mlambe05 <mark.lambert@fiscal.treasury.gov>
2020-03-02 14:17:34 -0500

    added historical data call

--- BinanceClient.py
+++ BinanceClient.py
@@ -1,15 +1,29 @@
 import requests
 import DateTime
+import time
+import json
 
 
 baseApiUrl = 'https://api.binance.us'
 connectivityEndpoint = '/api/v3/ping'
+klineDataEndpoint = '/api/v3/klines'
+testOrderEndpoint = '/api/v3/order/test'
 
 
+header = header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 def getHistoricalData():
-    returnVal = 'abcd'
-    return returnVal
+    url = baseApiUrl + klineDataEndpoint
+    data = {'symbol': 'BTCUSDT', 'interval': 'KLINE_INTERVAL_1MINUTE'}
+    jsonData = json.loads(data)
+    try:
+        response = requests.get(url, data=jsonData, header = header)
+        if(response.status_code == 200)
+            return response
+        else:
+            return 'recieved error code '+ response.status_code + ' when attempting to retrieve historical kline data'
+    except:
+        return 'error making historical data call to Binanace Exchange'
     
 def testConnectivity():
     url = baseApiUrl + connectivityEndpoint
