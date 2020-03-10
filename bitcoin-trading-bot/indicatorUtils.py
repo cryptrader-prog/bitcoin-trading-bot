5cba300a88ccbfb14f5f134f323cb15402338822
mlambe05 <mark.lambert@fiscal.treasury.gov>
2020-03-10 16:23:01 -0400

    added sqlite3 import, added function to create table for calcualting rsi data

--- bitcoin-trading-bot/indicatorUtils.py
+++ bitcoin-trading-bot/indicatorUtils.py
@@ -1,3 +1,24 @@
+import sqlite3
+
+#opens a database file, creates a new file if one doesn't exist exist
+db= sqlite3.connect('dataFile')
+
+#creates a table if one does not exist, otherwise does nothing
+def createTable():    
+    cursor = db.cursor()
+    try:
+        cursor.execute('''SELECT id FROM trade_data''')
+        data = cursor.fetchone()
+    except:
+        pass
+    if(data):
+        try:
+            cursor.execute('''CREATE TABLE trade_data(id INTEGER PRIMARY KEY, rsi REAL) ''')
+            db.commit()
+        except:
+            pass
+    else:
+        pass
 
 
 
--- bitcoin-trading-bot/tradingBot.py
+++ bitcoin-trading-bot/tradingBot.py
@@ -1,7 +1,9 @@
 import DiscordClient
 import BinanceClient
 import TwitterClient
+import indicatorUtils
 import log
+import time
 
 logger = log.setup_custom_logger('root')
 
@@ -11,62 +13,49 @@ def run():
     logger.info("This trading bot has been configured to use the Binance.us exchange.")
     logger.info("Trading pairs utilized are: BTC/USD")
     
-    #make call to Binance to gather trading data
-    logger.info(BinanceClient.getMarkets())
+    indicatorUtils.createTable()
 
-    tickerData = BinanceClient.getBinanceTickerData('BTC/USD')
-    logger.info("ticker Data: " + tickerData)
-    symbolname =tickerData.get('symbol')
-    marketHigh = tickerData.get('high')
-    marketLow = tickerData.get('low')
-    priceChange = tickerData.get('priceChangePercent')
+    while(True):
 
-    #prints current high and low for given symbol
-    message = "symbol ("+ str(symbolname) + ") data: currentHigh: " + str(marketHigh) + " currentLow: " + str(marketLow) + " PriceChange: " + str(priceChange)
+        #make call to Binance to gather trading data
+        logger.info(BinanceClient.getMarkets())
 
-    #sends symbol data to discord channel
-    logger.info("calling webhook with ticker data, check your discord!")
-    DiscordClient.callWebhook("ticker Data: " + message)
+        tickerData = BinanceClient.getBinanceTickerData('BTC/USD')
+        logger.info("ticker Data: " + tickerData)
+        symbolname =tickerData.get('symbol')
+        marketHigh = tickerData.get('high')
+        marketLow = tickerData.get('low')
+        priceChange = tickerData.get('priceChangePercent')
 
-    #sends symbol data to twitter account
-    # print("sending ticker data, check your twitter!")
-    # TwitterClient.sendToTwitter("ticker Data: " + message)
+        #prints current high and low for given symbol
+        message = "symbol ("+ str(symbolname) + ") data: currentHigh: " + str(marketHigh) + " currentLow: " + str(marketLow) + " PriceChange: " + str(priceChange)
 
-    #gets current balances of all currencies and displays freely usable funds for BTC and USD
-    logger.info("getting balance")
-    balance = BinanceClient.getBalance()
-    availableBtcFunds = balance['BTC']
-    availableUsdFunds = balance['USD']
-    logger.info("available funds for trading: BTC = " + str(availableBtcFunds['free']) + ", USD = " + str(availableUsdFunds['free']))
+        #sends symbol data to discord channel
+        logger.info("calling webhook with ticker data, check your discord!")
+        DiscordClient.callWebhook("ticker Data: " + message)
 
-    #makes a test order to confirm able to place orders
-    logger.info("placing a test order")
-    testOrder = BinanceClient.makeTestOrder()
-    logger.info(testOrder)
+        #sends symbol data to twitter account
+        # print("sending ticker data, check your twitter!")
+        # TwitterClient.sendToTwitter("ticker Data: " + message)
 
+        #gets current balances of all currencies and displays freely usable funds for BTC and USD
+        logger.info("getting balance")
+        balance = BinanceClient.getBalance()
+        availableBtcFunds = balance['BTC']
+        availableUsdFunds = balance['USD']
+        logger.info("available funds for trading: BTC = " + str(availableBtcFunds['free']) + ", USD = " + str(availableUsdFunds['free']))
 
-    
-    
+        #makes a test order to confirm able to place orders
+        logger.info("placing a test order")
+        testOrder = BinanceClient.makeTestOrder()
+        logger.info(testOrder)
 
-
-
-
-
-
-
-
-
-
-
-
+        #script sleeps for one minute before next run
+        time.sleep(60)
 
 
 
 run()
 
 
-#when ready, switch to the below method to run the job continuously
-'''
-while(True):
-    run()
-'''
+
